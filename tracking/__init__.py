b03fa8d917fd58f6e9a1271b45c27ef8ba9ee1a8
bashu <basil.shubin@gmail.com>
2015-08-05 11:34:18 +0600

    update setup.py

--- tracking/__init__.py
+++ tracking/__init__.py
@@ -2,7 +2,5 @@ import listeners
 
 VERSION = (0, 4, 0)
 
-def get_version():
-    "Returns the version as a human-format string."
-    return '.'.join([str(i) for i in VERSION])
+__version__ = '.'.join([str(n) for n in VERSION])
 
